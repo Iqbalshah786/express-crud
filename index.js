30c9d7bb4eab60bf9c72d6583c35f2fdf38d0a2b
Iqbal Shah <iqbalshahandiri786@gmail.com>
2024-06-19 11:40:43 +0500

    Add morgan and winston dependencies, and create logger utility

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 import express from "express";
 import "dotenv/config";
+import logger from "./utils/logger.js";
+import morgan from "morgan";
 
 const app = express();
 
 const port = process.env.PORT || 3000;
 app.use(express.json());
+
+const morganFormat = ":method :url :status :response-time ms";
+
+// middlewear
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
 
 let teaData = [];
 let nextId = 1;
@@ -15,6 +36,7 @@ app.get("/", (req, res) => {
 
 // add a new tea
 app.post("/teas", (req, res) => {
+  // logger.info("A post request is made to add a new tea");
   const { name, price } = req.body;
   const newTea = { id: nextId++, name, price };
   teaData.push(newTea);
